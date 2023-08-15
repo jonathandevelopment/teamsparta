76c10a334604e5868bedaa66971bb673c03fc6b8
Jonathancodesurfer <jonathanalpizar43@gmail.com>
2023-08-15 13:35:36 -0600

    metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,26 @@
+"use client";
+
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import Head from 'next/head';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 const inter = Inter({ subsets: ['latin'] })
 
-export const metadata: Metadata = {
-  title: 'Roles Griegos',
-  description: 'Todo en Roles ',
-  openGraph: {
-    title: 'Roles Griegos',
-    description: 'Todo en Roles Industriales',
-    siteName: 'Roles Griegos',
-    images: "/images/logo.png",
-    type: "website",
-    locale: "en_US",
-  },
-}
+// export const metadata: Metadata = {
+//   title: 'Roles Griegos',
+//   description: 'Todo en Roles ',
+//   openGraph: {
+//     title: 'Roles Griegos',
+//     description: 'Todo en Roles Industriales',
+//     siteName: 'Roles Griegos',
+//     images: "/images/logo.png",
+//     type: "website",
+//     locale: "en_US",
+//   },
+// }
 
 export default function RootLayout({
   children,
@@ -24,7 +29,26 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <Head>
+        <title>
+         Roles Griegos
+        </title>
+        <meta
+          name="description"
+          content="Especialistas en Soluciones Industriales: 
+          Explora Nuestra Amplia Gama de Productos: Rodamientos, Retenedores, 
+          Fajas, Bocinas, Fibras, Rótulas, Mangueras y Mucho Más."
+        />
+        <meta
+          property="og:image"
+          content="/images/logo.png"
+        />
+      </Head>
+      <body className={inter.className}>
+        <Navbar />
+        {children}
+        <Footer/>
+        </body>
     </html>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ import Footer from './components/Footer';
 export default function Home() {
   return (
     <div>
-      <Navbar/>
+      {/* <Navbar/> */}
       <Masthead/>
       <FeaturedProducts/>
       <Brands/>
