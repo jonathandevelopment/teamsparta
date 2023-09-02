ccd1217968d48270794f6d289a953bc1443b284b
Jonathancodesurfer <jonathanalpizar43@gmail.com>
2023-09-02 16:16:14 -0600

    added structure

---
+++ src/app/utils/products.ts
@@ -0,0 +1,69 @@
+
+export const products = [
+    {
+      id: 1,
+      name: "Jiujitsu Brasileño",
+      href: "#",
+      price: "$50",
+      description:
+        "Clases de Jiujitsu Brasileño para principiantes y avanzados. Aprende las técnicas de lucha y sumisión.",
+      options: "Clases grupales y privadas disponibles",
+      imageSrc: "/images/bjj.png",
+      imageAlt: "Imagen de una clase de Jiujitsu Brasileño.",
+    },
+    {
+      id: 2,
+      name: "Muay Thai",
+      href: "#",
+      price: "$40",
+      description:
+        "Clases de Muay Thai para mejorar tu resistencia y habilidades de combate. Entrenamiento completo.",
+      options: "Clases para todas las edades",
+      imageSrc: "/images/muaythai.png",
+      imageAlt: "Imagen de una clase de Muay Thai.",
+    },
+    {
+      id: 3,
+      name: "Kick Boxing",
+      href: "#",
+      price: "$35",
+      description:
+        "Clases de Kick Boxing para quemar calorías y desarrollar habilidades de combate. Apto para todos los niveles.",
+      options: "Instructores certificados",
+      imageSrc: "/images/kickbox.png",
+      imageAlt: "Imagen de una clase de Kick Boxing.",
+    },
+    {
+      id: 4,
+      name: "Boxeo",
+      href: "#",
+      price: "$30",
+      description:
+        "Clases de Boxeo para mejorar tu fuerza y coordinación. Entrenamiento intensivo con sacos y guantes.",
+      options: "Clases individuales y en grupo",
+      imageSrc: "/images/box.png",
+      imageAlt: "Imagen de una clase de Boxeo.",
+    },
+    {
+      id: 5,
+      name: "MMA",
+      href: "#",
+      price: "$60",
+      description:
+        "Entrenamiento completo en Artes Marciales Mixtas (MMA) con expertos instructores. Aprende a pelear en todas las distancias.",
+      options: "Entrenamiento personalizado disponible",
+      imageSrc: "/images/mma.png",
+      imageAlt: "Imagen de una clase de MMA.",
+    },
+    {
+      id: 6,
+      name: "Clases de Artes Marciales para Niños",
+      href: "#",
+      price: "$25",
+      description:
+        "Clases de artes marciales diseñadas específicamente para niños. Fomenta la disciplina, la confianza y la actividad física.",
+      options: "Grupos de edad adaptados",
+      imageSrc: "/images/kids.png",
+      imageAlt: "Imagen de una clase de artes marciales para niños.",
+    },
+  ];
